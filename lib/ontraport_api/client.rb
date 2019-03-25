cb59af8deaf74b8841a2f33bae7b057955a02159
Kirill Chernobai <evilzorge@gmail.com>
2019-03-25 20:14:21 +0300

    Add error check for invalid credentials

--- lib/ontraport_api/client.rb
+++ lib/ontraport_api/client.rb
@@ -62,6 +62,7 @@ module OntraportApi
     def query(method, path, payload = {})
       raise InvalidAPIMethodOrPath if [method, path].any? { |w| w !~ blank_regex } || ![:get, :post, :put, :delete].include?(method)
       response = self.class.send(method, path, query: payload, body: payload, headers: api_credentials_headers )
+      raise InvalidAppIdOrApiKey if response.code == 401
       response.parsed_response['data']
     rescue JSON::ParserError => e
       {
